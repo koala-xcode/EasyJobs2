1c9f131b5568ece791f15b31adb9eb1ec292b522
Myo <mkyaw24@googlemail.com>
2012-03-18 20:55:36 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyjobs::Application.config.secret_token = '4725c739f1b63915653c7d768ee1ae2aeecb400a8902b356f73403a3c52f5e53fb5b090443b15f776ea3c14e12b808363ea6b97ddaddb1199cd1f5895d8c0ffd'
